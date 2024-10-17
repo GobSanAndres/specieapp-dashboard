2bd71839530d57aed2e2104ca43d85b50acf8952
Jose Ruiz Urango <joseurango777@gmail.com>
2024-10-17 01:51:34 -0500

    test: blank space

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: true, 
   ssaypBackendUrl: 'https://ssayp-backend.rgpsai.org',
   port: 443,
   rgpBackendUrl: 'https://backend.rgpsai.org',
